34bf03d72817f24b5332660feeb32d120de36bc9
Shubham Singh <shubham2101@gmail.com>
2024-03-26 13:02:19 +0530

    separate data file

---
+++ data.js
@@ -0,0 +1,60 @@
+const characters = [
+  {
+    name: 'Luke Skywalker',
+    height: 172,
+    mass: 77,
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    name: 'Darth Vader',
+    height: 202,
+    mass: 136,
+    eye_color: 'yellow',
+    gender: 'male',
+  },
+  {
+    name: 'Leia Organa',
+    height: 150,
+    mass: 49,
+    eye_color: 'brown',
+    gender: 'female',
+  },
+  {
+    name: 'Anakin Skywalker',
+    height: 188,
+    mass: 84,
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    name: 'Yoda',
+    height: 66,
+    mass: 17,
+    eye_color: 'green',
+    gender: 'male',
+  },
+  {
+    name: 'Padmé Amidala',
+    height: 165,
+    mass: 45,
+    eye_color: 'brown',
+    gender: 'female',
+  },
+  {
+    name: 'Obi-Wan Kenobi',
+    height: 182,
+    mass: 81,
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    name: 'Mace Windu',
+    height: 194,
+    mass: 89,
+    eye_color: 'brown',
+    gender: 'male',
+  },
+];
+
+export { characters };
--- script.js
+++ script.js
@@ -1,61 +1,4 @@
-const characters = [
-  {
-    name: 'Luke Skywalker',
-    height: 172,
-    mass: 77,
-    eye_color: 'blue',
-    gender: 'male',
-  },
-  {
-    name: 'Darth Vader',
-    height: 202,
-    mass: 136,
-    eye_color: 'yellow',
-    gender: 'male',
-  },
-  {
-    name: 'Leia Organa',
-    height: 150,
-    mass: 49,
-    eye_color: 'brown',
-    gender: 'female',
-  },
-  {
-    name: 'Anakin Skywalker',
-    height: 188,
-    mass: 84,
-    eye_color: 'blue',
-    gender: 'male',
-  },
-  {
-    name: 'Yoda',
-    height: 66,
-    mass: 17,
-    eye_color: 'green',
-    gender: 'male',
-  },
-  {
-    name: 'Padmé Amidala',
-    height: 165,
-    mass: 45,
-    eye_color: 'brown',
-    gender: 'female',
-  },
-  {
-    name: 'Obi-Wan Kenobi',
-    height: 182,
-    mass: 81,
-    eye_color: 'blue',
-    gender: 'male',
-  },
-  {
-    name: 'Mace Windu',
-    height: 194,
-    mass: 89,
-    eye_color: 'brown',
-    gender: 'male',
-  },
-];
+import { characters } from './data.js';
 
 //***MAP***
 //1. Get array of all names
@@ -181,7 +124,7 @@ const sortByGender = characters.toSorted((a, b) => {
   }
   return 0; // Genders are equal
 });
-// console.log(sortByGender);
+console.log(sortByGender);
 
 //***EVERY***
 //1. Does every character have blue eyes?
