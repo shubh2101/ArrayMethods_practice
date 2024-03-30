910f5227023d67e47c8b593f7fa3b7a98e100d55
Shubham Singh <shubham2101@gmail.com>
2024-03-30 20:59:12 +0530

    array to string

--- script2.js
+++ script2.js
@@ -7,3 +7,15 @@ const is_array = (arr) => {
 function isArray(input) {
   return input instanceof Array;
 }
+
+//Write a simple JavaScript program to join all elements of the following array into a string.
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :
+//"Red,Green,White,Black"
+//"Red,Green,White,Black"
+//"Red+Green+White+Black"
+
+const myColor = ['Red', 'Green', 'White', 'Black'];
+const test1 = myColor.toString();
+console.log(myColor.join());
+console.log(myColor.join('+'));
